420661c812acb4d68c9eb95cd4860d6f84d6d8dd
Azmary <azmarydresti321@gmail.com>
2024-03-26 23:50:48 +0600

    Prepare base model added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from BrainTumorClassifier import logger
 from BrainTumorClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from BrainTumorClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -14,3 +14,14 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
